2b7a9e68ebcf4cb120411e1ee50a8abad1b00c6b
Jordan Cleigh <jordan.cleigh@fmgsuite.com>
2025-08-27 08:46:37 -0500

    * 'Line 6: Remove the using directive for 'Microsoft.Extensions.Configuration' as the 'Extensions' namespace is not available in .NET 9.0 or is no longer valid. Line 7: Remove the using directive for 'Microsoft.Extensions.Hosting' as the 'Extensions' namespace is not available in .NET 9.0 or is no longer valid.' in file 'tests\Catalog.FunctionalTests\GlobalUsings.cs'

--- tests/Catalog.FunctionalTests/GlobalUsings.cs
+++ tests/Catalog.FunctionalTests/GlobalUsings.cs
@@ -3,6 +3,4 @@ global using System.Net;
 global using System.Threading.Tasks;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.TestHost;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.Hosting;
 global using Xunit;
